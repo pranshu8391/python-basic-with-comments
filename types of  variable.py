05a0863f06ba6e35da9294a19126137949551909
Pratik Gulhane <166861943+pranshu8391@users.noreply.github.com>
2024-12-13 21:42:14 +0530

    Add files via upload

---
+++ types of  variable.py
@@ -0,0 +1,67 @@
+x = "hello world"   #STRING TYPE
+
+# display the value of x
+print(x)
+
+# display the type of x
+print(type(x))
+
+
+
+
+x = 20             #int type
+
+# display the value of x
+print(x)
+
+# display the type of x
+print(type(x))
+
+
+
+x = 20.5           # float type
+# display the value of x
+print(x)
+
+#display the type x
+print(type(x))
+
+
+
+x = 1j            # complex type
+
+# display the value of x
+print(x)
+
+#display the value type x
+print(type(x))
+
+
+
+x = ["apple", "banana", "orrange"]   # list type
+
+#display the value of variable x
+print(x)
+
+#display the type of variable x
+print(type(x))
+
+
+
+x = ("apple", "banana", "cherry")   # tupple type
+
+#diplay the value of x
+print(x)
+
+#display the type x
+print(type(x))
+
+
+
+x = range(6)
+
+#display the value of x
+print(x)
+
+#display the type of x
+print(type(x))
