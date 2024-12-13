07fb4f5a298447513c7d899435eb6f56c5336a4a
Pratik Gulhane <166861943+pranshu8391@users.noreply.github.com>
2024-12-13 23:37:39 +0530

    Add files via upload

---
+++ casting data types.py
@@ -0,0 +1,28 @@
+# python casting
+#int type
+x = int(1)    
+y = int(5.8)
+z = "22"
+
+print(x)
+print(y)
+print(z)
+
+
+# float type
+x = float(2)
+y = float(2.8)
+z = float("5")
+
+print(x)
+print(y)
+print(z)
+
+# string type
+x = str("6")
+y = str("23.20")
+z = str('21')
+
+print(x)
+print(y)
+print(z)
