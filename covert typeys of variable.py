a0d27b16549ad3384897bb184fa6e11eca8c16c0
Pratik Gulhane <166861943+pranshu8391@users.noreply.github.com>
2024-12-13 22:23:30 +0530

    Add files via upload

---
+++ covert typeys of variable.py
@@ -0,0 +1,31 @@
+# convert types
+
+x = 6
+y = 0.25
+z = 1j
+
+#convert from int to float
+a = float(x)
+
+# convert from float to int 
+b = int(y)
+
+# convert in to complex
+c = complex(x)
+
+
+print(a)
+print(b)
+print(c)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+
+# randome number
+import random
+
+print(random.randrange(1, 120))
